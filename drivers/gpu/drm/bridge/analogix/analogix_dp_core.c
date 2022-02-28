32c95fff496d3d0909c8fb5d49b4f762657363fb
Wyon Bi <bivvy.bi@rock-chips.com>
2022-02-28 09:16:37 +0800

    drm/rockchip: analogix_dp: Move resume handling to NOIRQ phase
    Fixes: 1a4c9d772a99 ("drm/bridge: analogix_dp: Rework irq handling")
    Signed-off-by: Wyon Bi <bivvy.bi@rock-chips.com>
    Change-Id: I8709ef94503a80e4c217f28b6d2855a2f4cedcd0

--- drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
@@ -1974,6 +1974,23 @@ void analogix_dp_remove(struct analogix_dp_device *dp)
 }
 EXPORT_SYMBOL_GPL(analogix_dp_remove);
 
+int analogix_dp_suspend(struct analogix_dp_device *dp)
+{
+	pm_runtime_force_suspend(dp->dev);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(analogix_dp_suspend);
+
+int analogix_dp_resume(struct analogix_dp_device *dp)
+{
+	pm_runtime_force_resume(dp->dev);
+	analogix_dp_init(dp);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(analogix_dp_resume);
+
 int analogix_dp_runtime_suspend(struct analogix_dp_device *dp)
 {
 	clk_bulk_disable_unprepare(dp->nr_clks, dp->clks);
