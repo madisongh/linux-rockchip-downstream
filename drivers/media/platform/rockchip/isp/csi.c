1caf220efb160a5cfc1977421c4c070b7d0bedff
Matt Madison <matt@madison.systems>
2023-06-28 11:54:32 -0700

    media: rockchip: isp: configure readback mode with multiple virtual ISPs
    Signed-off-by: Matt Madison <matt@madison.systems>

--- drivers/media/platform/rockchip/isp/csi.c
+++ drivers/media/platform/rockchip/isp/csi.c
@@ -593,7 +593,7 @@ int rkisp_csi_config_patch(struct rkisp_device *dev)
 				dev->hdr.op_mode = HDR_RDBK_FRAME1;
 
 			if (dev->isp_inp == INP_CIF && dev->isp_ver > ISP_V21)
-				mode.rdbk_mode = dev->is_rdbk_auto ? RKISP_VICAP_RDBK_AUTO : RKISP_VICAP_ONLINE;
+				mode.rdbk_mode = (dev->is_rdbk_auto || dev->hw_dev->dev_link_num > 1) ? RKISP_VICAP_RDBK_AUTO : RKISP_VICAP_ONLINE;
 			else
 				mode.rdbk_mode = RKISP_VICAP_RDBK_AIQ;
 			v4l2_subdev_call(mipi_sensor, core, ioctl, RKISP_VICAP_CMD_MODE, &mode);
