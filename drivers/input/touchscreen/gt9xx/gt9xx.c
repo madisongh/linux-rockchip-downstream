c5e22ff85e090a026edfa9c515e60d93fe6cd2b8
Liu Yuan <liuy@armdesigner.com>
2025-02-11 10:04:52 +0800

    debug TP gt928

--- drivers/input/touchscreen/gt9xx/gt9xx.c
+++ drivers/input/touchscreen/gt9xx/gt9xx.c
@@ -2654,7 +2654,7 @@ static int goodix_ts_probe(struct i2c_client *client, const struct i2c_device_id
 	if (val == 89) {
 		m89or101 = TRUE;
 		gtp_change_x2y = TRUE;
-		gtp_x_reverse = FALSE;
+		gtp_x_reverse = TRUE;
 		gtp_y_reverse = TRUE;
 	} else if (val == 101) {
 		m89or101 = FALSE;
@@ -2863,6 +2863,8 @@ probe_init_error:
     printk("   <%s>_%d  prob error !!!!!!!!!!!!!!!\n", __func__, __LINE__);    
     GTP_GPIO_FREE(ts->rst_pin);
     GTP_GPIO_FREE(ts->irq_pin);
+    return 0;
+    
 probe_init_error_requireio:
     tp_unregister_fb(&ts->tp); 
     kfree(ts);
